20afbf3b669c3a036414e3f6e5a336207e0f0956
OHOKs <85952515+OHOKs@users.noreply.github.com>
2023-11-13 18:19:36 +0100

    Add files via upload

--- Calculator/js.js
+++ Calculator/js.js
@@ -1,3 +1,13 @@
+let powered = false;
+
+function power(){
+    powered == false ? powered = true : powered = false;
+}
+
+function deleteLastCharacter(){
+
+}
+
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
@@ -6,32 +16,38 @@ function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
+function sqrt() {
+    var displayValue = eval(document.getElementById('display').value);
+    var result = Math.sqrt(displayValue);
+
+    document.getElementById('display').value = result;
+}
+
+function sin() {
+    var displayValue = eval(document.getElementById('display').value);
+    var result = Math.sin(displayValue);
+
+    document.getElementById('display').value = result;
+}
+
+function cos() {
+    var displayValue = eval(document.getElementById('display').value);
+    var result = Math.cos(displayValue);
+
+    document.getElementById('display').value = result;
+}
+
+function tan() {
+    var displayValue = eval(document.getElementById('display').value);
+    var result = Math.tan(displayValue);
+
+    document.getElementById('display').value = result;
+}
+
 function calculate() {
     try {
         document.getElementById('display').value = eval(document.getElementById('display').value);
     } catch {
         document.getElementById('display').value = 'Error';
     }
-}
-
-function calculateSqrt() {
-    var displayValue = eval(document.getElementById('display').value)
-    var result = Math.sqrt(displayValue);
-
-    document.getElementById('display').value = result;
-}
-
-function calculateSin() {
-    var result = Math.sin(eval(document.getElementById('display').value));
-    document.getElementById('display').value = result;
-}
-
-function calculateCos() {
-    var result = Math.cos(eval(document.getElementById('display').value));
-    document.getElementById('display').value = result;
-}
-
-function calculateTan() {
-    var result = Math.tan(eval(document.getElementById('display').value));
-    document.getElementById('display').value = result;
 }
